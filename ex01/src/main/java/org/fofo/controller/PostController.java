725d1bcdb192e5375b1e5e8e00162fb56c91aa7d
KaylaKwon <papermoon703@gmail.com>
2016-02-15 02:43:50 +0900

    이제 돌아간다아아아아아

--- ex01/src/main/java/org/fofo/controller/PostController.java
+++ ex01/src/main/java/org/fofo/controller/PostController.java
@@ -2,11 +2,14 @@ package org.fofo.controller;
 
 import javax.inject.Inject;
 
+import org.fofo.domain.PostVO;
 import org.fofo.service.PostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/post/*")
@@ -15,4 +18,22 @@ public class PostController {
 	
 	@Inject
 	private PostService service;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registerGET(PostVO post, Model model) throws Exception{
+		logger.info("register get~~~~~~~~~~~~~~~");
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registPOST(PostVO post, Model model) throws Exception{
+		logger.info("register post~~~~~~~~~~~~~~~~~");
+		logger.info(post.toString());
+		
+		service.regist(post);
+		
+		model.addAttribute("result", "success");
+		return "/board/success";
+	}
+	
+	
 }
